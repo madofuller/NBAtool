b892896424521cf466519a245e8b11729f61627a
matthew <mdf@fastmail.com>
2022-07-19 16:02:34 -0500

    pandas streamlit test

--- streamlit_app.py.py
+++ streamlit_app.py.py
@@ -1,12 +1,7 @@
-import mechanize as mc
-from bs4 import BeautifulSoup as bs
+import pandas as pd
+import numpy as np
 
 
-br = mc.Browser()
+pd.read_excel(NBAtool.xlsx, sheet_name='Consolidated')
 
-br.set_handle_robots(False) 
-br.open('https://invistavisibility.lightning.force.com/lightning/r/SalesAgreementAggregate__c/a0U4V00000GT6nOUAT/view')
-html = br.read()
-test = open('test.html', 'wb')
-print >> test, "%s" % (bs(html).prettify())  
-test.close()
+st.dataframe(data=pd.read_excel)
