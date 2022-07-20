2facd75ce00fef2d620783f50c700a5af4f16799
matthew <mdf@fastmail.com>
2022-07-20 15:14:45 -0500

    added sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,23 @@ import numpy as np
 
 st.title('NBA Tool')
 
+with st.sidebar:
+	product = st.selectbox(
+		'Choose Product',
+		('AA', 'HMD', 'ADN', 'Polymer'))
+	site = st.selectbox(
+		'Choose Site Location',
+		('North America', 'China', 'Rest of Asia'))
+	customer = st.selectbox(
+		'Choose Customer',
+		('North America', 'China', 'Rest of Asia'))
+	contract = st.selectbox(
+		'Choose Contract',
+		('North America', 'China', 'Rest of Asia'))
+	choose_date = st.date_input(
+    	"Choose Date",
+    	datetime.date(2022, 7, 1))
+	choose_customer
 
 df = pd.read_excel('NBA_Tool.xlsx', sheet_name='Consolidated')
 
