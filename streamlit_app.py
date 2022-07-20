f993bc021acf216e643374c1745cc1e449bfba46
matthew <mdf@fastmail.com>
2022-07-20 15:53:05 -0500

    added additional selectors

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,24 +8,19 @@ df = pd.read_excel('NBA_Tool.xlsx', sheet_name='Consolidated')
 
 product = df['Product'].drop_duplicates()
 
+site = df['Location'].drop_duplicates()
 
-
-
-
+contract = df['Contract_index'].drop_duplicates()
 
 
 with st.sidebar:
 	product_choice = st.selectbox(
 		'Choose Product', product)
 	site_choice = st.selectbox(
-		'Choose Site Location',
-		('North America', 'China', 'Rest of Asia'))
-	customer_choice = st.selectbox(
-		'Choose Customer',
-		('North America', 'China', 'Rest of Asia'))
+		'Choose Site Location', site)
 	contract_choice = st.selectbox(
 		'Choose Contract',
-		('North America', 'China', 'Rest of Asia'))
+		(contract))
 	date_choice = st.date_input(
     	"Choose Date")
 
