466210445d7d22cb58e8e238e470c4cde7966b22
matthew <mdf@fastmail.com>
2022-07-21 11:40:02 -0500

    revert

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ df = pd.read_excel('NBA_Tool.xlsx',
 	sheet_name='Consolidated')
 
 
-min_date = datetime.date(2020,1)
-max_date = datetime.date(2023,12)
+min_date = datetime.date(2020,1,1)
+max_date = datetime.date(2023,12,31)
 
 product = df['Product'].drop_duplicates()
 
