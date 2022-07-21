57fceaf30aeda8d56652b465edba7b31750b687e
matthew <mdf@fastmail.com>
2022-07-21 09:33:31 -0500

    removed !

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 	with st.sidebar:
 		product_choice = st.selectbox(
 			'Choose Product', options=['select']+list(df.keys())
-		if (product_choice) != 'select':
+		if (product_choice) = 'select':
 			site_choice = st.selectbox(
 				'Choose Site Location', options=df[product_choice])
 		contract_choice = st.selectbox(
